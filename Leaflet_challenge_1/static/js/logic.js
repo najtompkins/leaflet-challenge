ec73acb43e97bb9555ebb4402de98c3925b3476d
najtompkins <nathan.andrew.tomp@gmail.com>
2023-10-04 13:04:23 -0400

    Finalized code

--- Leaflet_challenge_1/static/js/logic.js
+++ Leaflet_challenge_1/static/js/logic.js
@@ -1,22 +1,33 @@
 // Load earthquake data from the USGS GeoJSON feed (replace with your dataset URL)
 fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson')
     .then(response => response.json())
+
+    // Process the data using an an arrow/lambda function
     .then(data => {
+
+        // log the data to explore it further in the console
+        // (Uncomment to use)
+        // console.log(data)
+
         // Loop through the earthquake data and create markers
         data.features.forEach(feature => {
             var coordinates = feature.geometry.coordinates;
             var magnitude = feature.properties.mag;
             var depth = coordinates[2];
+            var address = feature.properties.place;
 
             // Create a marker with popup
             var marker = L.circleMarker([coordinates[1], coordinates[0]], {
-                radius: (magnitude * 2),
+                radius: (magnitude * 3),
                 fillColor: getMarkerColor(depth),
                 color: 'gray',
                 weight: 1,
                 opacity: 1,
                 fillOpacity: 0.8
-            }).bindPopup(`<strong>Magnitude:</strong> ${magnitude}<br><strong>Depth:</strong> ${depth} km`);
+            }).bindPopup(`<strong>Magnitude:</strong> ${magnitude}<br>
+                            <strong>Depth:</strong> ${depth} km<br>
+                            <strong>Location:</strong> ${address}<br>
+                            `);
 
             // Add the marker to the map
             marker.addTo(map);
@@ -47,12 +58,12 @@ function getMarkerColor(depth) {
     }
 }
 
-
-
 // Create a legend
 var legend = L.control({ position: 'bottomright' });
 legend.onAdd = function () {
     var div = L.DomUtil.create('div', 'info legend');
+
+    div.innerHTML = '<h4>Quake Depth</h4>'; // Add the title
     var depths = [0, 10, 30, 50, 70];
     var labels = ['green', 'yellow', 'orange', 'red','darkred'];
 
